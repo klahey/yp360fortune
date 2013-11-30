5d47dcd99eca8c426d940194b04e78e200cc2190
Karen Lahey <karen@laheyfamily.org>
2013-11-30 09:02:19 -0800

    Initialize app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Yp360fortune::Application.config.secret_key_base = 'ac9cacd3f0b08618ca6bf1dbb34b69d00fccf78fc9ca93632b8e292ce6e830c2605cee0e8ac92216c76dcd9dd6accf5dd86ebab98e264858bb182b79cf3bd04f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Yp360fortune::Application.config.session_store :cookie_store, key: '_yp360fortune_session'
